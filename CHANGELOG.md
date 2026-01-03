ac7d306c1e401970de67b5f8de61c59cc2f566d4
zkhssb <z164907681@gmail.com>
2026-01-03 13:44:44 +0800

    上游针对BridgeClient的优化 [0.1.0-rc.4]

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+### 0.1.0-rc.4
+
+#### Bridge [1.6]
+
+- optimize: 优化Bridge重连模式
+
 ### 0.1.0-rc.3
 
 - fix: 修复NeoForge1.21部分版本无法加载的问题
--- src/main/java/com/springwater/easybot/platforms/ModData.java
+++ src/main/java/com/springwater/easybot/platforms/ModData.java
@@ -6,7 +6,7 @@ import org.slf4j.LoggerFactory;
 public class ModData {
     public static final String MOD_ID = /*$ mod_id*/ "easybot"; // 别删前面的注释,删了我打屎你们
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-    public static final String VERSION = /*$ mod_version*/ "0.1.0-rc.2"; // 别删前面的注释,删了我打屎你们
+    public static final String VERSION = /*$ mod_version*/ "0.1.0-rc.4"; // 别删前面的注释,删了我打屎你们
     public static final String MINECRAFT = /*$ minecraft*/ "1.20.1";   // 别删前面的注释,删了我打屎你们
     public static final String PAPI = /*$ papi*/ "2.1.4+1.20.1";   // 别删前面的注释,删了我打屎你们
     public static final String LOADER = /*$ loader*/ "a";
